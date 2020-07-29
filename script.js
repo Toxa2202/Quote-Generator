dd1b0c48efc4113322b3f4c83dac93890f63ae3d
Anthony Saint <Svyatoj@gmail.com>
2020-07-29 20:44:28 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 // Get Quote From API
 async function getQuote() {
-    const proxyUrl = 'https://protected-dusk-18335.herokuapp.com/'
+    const proxyUrl = 'https://toxa2202.github.io/quote-generator/'
     const apiUrl = 'http://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=json';
     try {
         const response = await fetch(proxyUrl + apiUrl);
